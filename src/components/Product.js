de5d8047027bf483a4410046d98d4ee4b3b75c68
Bill Humphrey <123195672+billhumphrey@users.noreply.github.com>
2025-06-20 17:45:48 +0300

    Add files via upload

---
+++ src/components/Product.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import {useEffect} from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import { useDispatch, useSelector } from 'react-redux';
+import { add } from '../store/cartSlice';
+import { getProducts } from '../store/productSlice';
+
+
+function Product() {
+  const dispatch = useDispatch();
+  const {data: products} = useSelector(state => state.products);
+
+  useEffect(() => {
+   
+   dispatch(getProducts());   
+    // api
+    // fetch('https://fakestoreapi.com/products')
+    // .then(data => data.json())
+    // .then(result => getProducts(result))
+
+  }, []);
+
+  const addToCart = (product) => {
+    // dispatch an add action
+    dispatch(add(product))
+  }
+
+
+  const cards = products.map(product => (
+    <div className='col-md-3' style={{ marginBottom: '10px' }}>
+      <Card key={product.id} className='h-100'>
+        <div>
+      <Card.Img variant="top" src={product.image} style={{ width : '100px', height: '130px'}}/>
+      </div>
+      <Card.Body>
+        <Card.Title>{product.title}</Card.Title>
+        <Card.Text>
+          KSH: {product.price}
+        </Card.Text>
+      </Card.Body>
+
+      <Card.Footer style={{ background : 'white' }}>
+      <Button variant="primary" onClick={() => addToCart(product)}>Add To Cart</Button>
+      </Card.Footer>
+    </Card>
+    </div>
+  ))
+
+  return (
+    <>
+    <h1>Product Dashboard</h1>
+    <div className='row'>
+      {cards}
+    </div>
+    </>
+  )
+}
+
+export default Product;
